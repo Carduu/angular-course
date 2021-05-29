a43f0084fed1b81f153d88a0e394a4877523e38e
Carduu <jose_101195@hotmail.com>
2021-05-28 21:45:55 -0300

    Component Fundamentals & Form Template Exercises

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,12 +13,47 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'JavaScript: The Hard Parts',
+      description: 'Improve your JS knowledge',
+      percentComplete: 100,
+      favorite: true
+    },
+    {
+      id: 3,
+      title: 'Getting Started with JavaScript',
+      description: 'Learn JS basics',
+      percentComplete: 75,
+      favorite: false
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+      this.resetSelectedCourse();
   }
 
+  selectedCourse = null;
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
+
+  resetSelectedCourse() {
+      const emptyCourse = {
+          id: null,
+          title: '',
+          description: '',
+          percentComplete: 0,
+          favorite: false
+      };
+      this.selectedCourse = emptyCourse;
+  }
+
+  saveCourse() {
+      console.log('Submitted course!');
+  }
 }
